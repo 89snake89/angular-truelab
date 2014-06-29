db76fc64a185d7122803e5bf3b3c2eb504627b70
Ruben Barilani <ruben.barilani.dev@gmail.com>
2014-06-29 17:54:05 +0200

    Updates dist - 2014-06-29 17:53

--- angular-truelab.min.js
+++ angular-truelab.min.js
@@ -1,7 +1,7 @@
 /**
  * @name angular-truelab
  * @description angular-truelab - Truelab angular modules
- * @version v0.0.0 - 2014-06-28 17:18
+ * @version v0.0.0 - 2014-06-29 17:53
  * @link http://truelab.github.io/angular-truelab
  * @license MIT License, http://www.opensource.org/licenses/MIT
  **/
